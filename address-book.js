9e3b0e029484f8ffd53850a1c92ea30375405766
jwhear93 <jaweebelow420@gmail.com>
2017-05-01 16:47:21 +0100

    Tidy JS - no changes

--- address-book.js
+++ address-book.js
@@ -1,41 +1,3 @@
-////        make printable string for console readout, recursively
-var make_printable_object = function(ar_use)
-{
-////        internal arguments
-var in_tab = arguments[1];
-var st_return = arguments[2];
-////        default vales when applicable
-if (!in_tab) in_tab = 0;
-if (!st_return) st_return = "";
-////        add depth
-var st_tab = "";
-for (var i=0; i < in_tab; i++) st_tab = st_tab+"-~-~-";
-
-////        traverse given depth and build string
-for (var key in ar_use)
-{
-    ////        gather return type
-    var st_returnType = typeof ar_use[key];
-    ////        get current depth display
-    var st_returnPrime = st_tab+ "["+key+"] ->"+ar_use[key]+"< is {"+st_returnType+"}"+ '<br>';
-    ////        remove linefeeds to avoid printout confusion
-    
-    ////        add line feed
-    st_return = st_return+st_returnPrime+"\n";
-    ////         stop at a depth of 15
-    if (in_tab>15) return st_return;
-    ////        if current value is an object call this function
-    if ( (typeof ar_use[key] == "object") & (ar_use[key] != "null") & (ar_use[key] != null) ) st_return = make_printable_object(ar_use[key], in_tab+1, st_return);
-
-
-}
-
-////        return complete output
-return st_return;
-
-};
-////        END
-
 ////    Console log to HTML
 (function () {
     var old_logger = console.log;
@@ -71,6 +33,7 @@ String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
+/*Add new contact*/
 function add (firstName, lastName, phoneNumber, email) {
     contacts[contacts.length] = {
         firstName: firstName,
@@ -84,6 +47,7 @@ function printPerson(person) {
     console.log(person.firstName + " - " + person.lastName + " - " + person.phoneNumber + " - " + person.email);
 }
 
+/*List function*/
 function list() {
     var contactsLength = contacts.length;
     for (var i = 0; i < contactsLength; i++) {
